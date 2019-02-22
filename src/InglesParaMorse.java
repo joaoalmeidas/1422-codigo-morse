44a59b6bf4b84f793afe5fb13685dfd999ac801f
joao <joao182cx@gmail.com>
2019-02-22 15:44:51 -0300

    Aprimoramento do código que faz a leitura do código morse.

--- src/InglesParaMorse.java
+++ src/InglesParaMorse.java
@@ -8,10 +8,33 @@ public class InglesParaMorse {
 		String morse;
 		
 		String[] palavrasMorse;
-		String[] letrasMorse;
+		String[][] letrasMorse;
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Insira uma mensagem em código morse:");
+		morse = input.nextLine();
+		palavrasMorse = morse.split("   ");
+		
+		letrasMorse = new String[palavrasMorse.length][];
+		
+		for(int i = 0; i < palavrasMorse.length; i++) {
+				
+			letrasMorse[i] = palavrasMorse[i].split(" ");
+			
+		}
+		
+		for(int i = 0; i < letrasMorse.length; i++) {
+			
+			for(int j = 0; j < letrasMorse[i].length; j++) {
+				
+				System.out.printf(letrasMorse[i][j]);
+				
+			}
+			
+			System.out.println();
+			
+		}
 
 	}
 
