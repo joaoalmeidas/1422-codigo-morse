08a5a5ac4464927382464c2484b84f4453876dac
joao <joao182cx@gmail.com>
2019-02-22 14:47:57 -0300

    Criação do código que faz a leitura do código morse.

---
+++ src/InglesParaMorse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class InglesParaMorse {
+
+	public static void main(String[] args) {
+		
+		String ingles;
+		String morse;
+		
+		String[] palavrasMorse;
+		String[] letrasMorse;
+		
+		Scanner input = new Scanner(System.in);
+		
+
+	}
+
+}
