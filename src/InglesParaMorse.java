ce8468e42141211143ebab077833a662f1f92fe4
joao <joao182cx@gmail.com>
2019-02-22 16:10:22 -0300

    Criação do código que traduz cada código de caractere de código morse digitado pelo usuário e traduz para uma letra ou dígito.

--- src/InglesParaMorse.java
+++ src/InglesParaMorse.java
@@ -4,7 +4,7 @@ public class InglesParaMorse {
 
 	public static void main(String[] args) {
 		
-		String ingles;
+		String ingles = "";
 		String morse;
 		
 		String[] palavrasMorse;
@@ -28,13 +28,160 @@ public class InglesParaMorse {
 			
 			for(int j = 0; j < letrasMorse[i].length; j++) {
 				
-				System.out.printf(letrasMorse[i][j]);
+				if(letrasMorse[i][j].equals(".-")) {
+					
+					ingles += "A";
+					
+				}else if(letrasMorse[i][j].equals("-...")) {
+					
+					ingles += "B";
+					
+				}else if(letrasMorse[i][j].equals("-.-.")) {
+					
+					ingles += "C";
+					
+				}else if(letrasMorse[i][j].equals("-..")) {
+					
+					ingles += "D";
+					
+				}else if(letrasMorse[i][j].equals(".")) {
+					
+					ingles += "E";
+					
+				}else if(letrasMorse[i][j].equals("..-.")) {
+					
+					ingles += "F";
+					
+				}else if(letrasMorse[i][j].equals("--.")) {
+					
+					ingles += "G";
+					
+				}else if(letrasMorse[i][j].equals("....")) {
+					
+					ingles += "H";
+					
+				}else if(letrasMorse[i][j].equals("..")) {
+					
+					ingles += "I";
+					
+				}else if(letrasMorse[i][j].equals(".---")) {
+					
+					ingles += "J";
+					
+				}else if(letrasMorse[i][j].equals("-.-")) {
+					
+					ingles += "K";
+					
+				}else if(letrasMorse[i][j].equals(".-..")) {
+					
+					ingles += "L";
+					
+				}else if(letrasMorse[i][j].equals("--")) {
+					
+					ingles += "M";
+					
+				}else if(letrasMorse[i][j].equals("-.")) {
+					
+					ingles += "N";
+					
+				}else if(letrasMorse[i][j].equals("---")) {
+					
+					ingles += "O";
+					
+				}else if(letrasMorse[i][j].equals(".--.")) {
+					
+					ingles += "P";
+					
+				}else if(letrasMorse[i][j].equals("--.-")) {
+					
+					ingles += "Q";
+					
+				}else if(letrasMorse[i][j].equals(".-.")) {
+					
+					ingles += "R";
+					
+				}else if(letrasMorse[i][j].equals("...")) {
+					
+					ingles += "S";
+					
+				}else if(letrasMorse[i][j].equals("-")) {
+					
+					ingles += "T";
+					
+				}else if(letrasMorse[i][j].equals("..-")) {
+					
+					ingles += "U";
+					
+				}else if(letrasMorse[i][j].equals("...-")) {
+					
+					ingles += "V";
+					
+				}else if(letrasMorse[i][j].equals(".--")) {
+					
+					ingles += "W";
+					
+				}else if(letrasMorse[i][j].equals("-..-")) {
+					
+					ingles += "X";
+					
+				}else if(letrasMorse[i][j].equals("-.--")) {
+					
+					ingles += "Y";
+					
+				}else if(letrasMorse[i][j].equals("--..")) {
+					
+					ingles += "Z";
+					
+				}else if(letrasMorse[i][j].equals(".----")) {
+					
+					ingles += "1";
+					
+				}else if(letrasMorse[i][j].equals("..---")) {
+					
+					ingles += "2";
+					
+				}else if(letrasMorse[i][j].equals("...--")) {
+					
+					ingles += "3";
+					
+				}else if(letrasMorse[i][j].equals("....-")) {
+					
+					ingles += "4";
+					
+				}else if(letrasMorse[i][j].equals(".....")) {
+					
+					ingles += "5";
+					
+				}else if(letrasMorse[i][j].equals("-....")) {
+					
+					ingles += "6";
+					
+				}else if(letrasMorse[i][j].equals("--...")) {
+					
+					ingles += "7";
+					
+				}else if(letrasMorse[i][j].equals("---..")) {
+					
+					ingles += "8";
+					
+				}else if(letrasMorse[i][j].equals("----.")) {
+					
+					ingles += "9";
+					
+				}else if(letrasMorse[i][j].equals("-----")) {
+					
+					ingles += "0";
+					
+				}
 				
 			}
 			
-			System.out.println();
+			ingles += " ";
 			
 		}
+		
+		System.out.println(ingles);
+		
 
 	}
 
